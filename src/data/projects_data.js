454cfdc4c35cc32dd3a428d64ad61765c012ea7a
Kviraj <kawa.135viraj@gmail.com>
2024-03-10 12:06:02 +0530

    feat: Resume update and project add.

--- src/data/projects_data.js
+++ src/data/projects_data.js
@@ -1,6 +1,6 @@
 import FileSharing from "../assets/images/Screenshot from 2024-01-08 21-30-30.png";
-import DevDetective from "../assets/images/Screenshot from 2024-01-08 21-35-12.png"
-import StudyNotion from "../assets/images/Screenshot from 2024-01-08 21-29-02.png";
+import DevDetective from "../assets/images/Screenshot from 2024-01-08 21-35-12.png";
+import PromptAi from "../assets/images/image.png";
 
 export const projects = [
   {
@@ -12,11 +12,11 @@ export const projects = [
   },
 
   {
-    thumbnail: StudyNotion,
-    title: "StudyNotion An Ed-tech platform",
-    content: `StudyNotion is a robust and fully functional ed-tech platform designed to empower both students and instructors. It allows users to create, consume, and rate educational content, fostering an enriched learning experience. With its user-friendly interface and cutting-edge features, StudyNotion aims to make education more accessible, engaging, and interactive. Features`,
-    liveUrl: "https://github.com/Kviraj722/StudyNotion-An-Edtech",
-    tools: ["MongoDb","ExpressJs","React", "NodeJs"],
+    thumbnail: PromptAi,
+    title: "Prompt AI",
+    content: `Prompt AI is a robust and fully functional prompt sharing application. Share your prompts with world.`,
+    liveUrl: "https://prompt-ai-virajkawa.vercel.app/",
+    tools: ["NextJs", "Tailwind", "MongoDB"],
   },
 
   {
@@ -25,9 +25,8 @@ export const projects = [
     content: `
 
     DevDetective is a web application built using HTML, CSS, and JavaScript. The application allows users to search for GitHub profiles and display basic information about the user, including their avatar, username, bio, number of followers, and number of repositories. The application makes use of the GitHub API to retrieve the necessary data.`,
-    liveUrl: "https://dev-detective-ekclcic2n-virajs-projects-4f585551.vercel.app/",
+    liveUrl:
+      "https://dev-detective-ekclcic2n-virajs-projects-4f585551.vercel.app/",
     tools: ["HTML", "CSS", "JavaScript", "Github APIs"],
   },
-
-
 ];
